48d74c1d1d6eec2e6a03e25c1fe1016f60faf6aa
pwang96 <96pwang@gmail.com>
2018-09-08 15:17:13 -0400

    adr

--- algo/MarketMaking.py
+++ algo/MarketMaking.py
@@ -17,11 +17,11 @@ class MarketMaking(Algo):
             # if -50 <= self.positions[security] <= 50:
             #     trades.append((security, curr_spread[0] + 1, 20))
             #     trades.append((security, curr_spread[1] - 1, -20))
-            if avg30tick + 2 * stddev < avg:
+            if avg30tick + 1 * stddev < avg:
                 if -50 <= self.positions[security] <= 50:
                     trades.append((security, curr_spread[0] + 1, 20))
                     trades.append((security, curr_spread[1] + 2, -20))
-            elif avg30tick - 2 * stddev > avg:
+            elif avg30tick - 1 * stddev > avg:
                 if -50 <= self.positions[security] <= 50:
                     trades.append((security, curr_spread[0] - 2, 20))
                     trades.append((security, curr_spread[1] - 1, -20))
